9d22b13e8a5d14bf83c9d059d70c0cfe1b47986f
audreylky <audrey@nextacademy.com>
2017-04-06 10:10:35 +0800

    Created migration for student table

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,24 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, format: { with: /.@.+\...+/, message: "this is not a valid email" }
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than: 5 }
+  validates :phone, format: { with: /.*\d{3}.*\d{3}.*\d{4}.*/, message: "this is not a valid phone" }
 
+	def name
+		name = self.first_name + " " + self.last_name
+	end
+
+	def age
+		age = Date.today.year - self.birthday.year
+
+		if self.birthday.month == Date.today.month && self.birthday.day > Date.today.day  # same birthday month, but haven't reach bday date!
+			age -= 1
+		elsif self.birthday.month > Date.today.month 	# haven't reach birthday month yet
+			age -= 1
+		else
+			age
+		end
+	end
 end
